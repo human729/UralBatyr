a66a533eb10a474f67767952f4fc5ac4eed5adfb
Alone_ight <alb.nurm@gmail.com>
2025-11-10 23:59:33 +0500

    tp

---
+++ Assets/Scripts/TeleportTrigger.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class TeleportTrigger : MonoBehaviour
+{
+    public Transform[] teleportPoints;
+    public KeyCode interactionKey = KeyCode.E;
+
+    private bool playerInTrigger = false;
+    private Vector3 currentPosition;
+
+    private void Start()
+    {
+        currentPosition = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInTrigger && Input.GetKeyDown(interactionKey))
+        {
+            TeleportToRandomPoint();
+        }
+    }
+
+    private void TeleportToRandomPoint()
+    {
+        if (teleportPoints.Length == 0) return;
+        List<Transform> availablePoints = new List<Transform>();
+
+        foreach (Transform point in teleportPoints)
+        {
+            if (point != null && point.position != currentPosition)
+            {
+                availablePoints.Add(point);
+            }
+        }
+
+        if (availablePoints.Count == 0) return;
+
+        Transform randomPoint = availablePoints[Random.Range(0, availablePoints.Count)];
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            Transition.Instance.TeleportPlayer(randomPoint.position);
+        }
+    }
+}
