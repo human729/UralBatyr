a80c4305f3f839f4230421aec4965739b0752209
oLEG325234 <OlegOlegov236jshg@GMAIL.COM>
2025-11-11 17:45:20 +0500

    asd

--- Assets/Scripts/PlayerScripts/EnemyBehaviour.cs
+++ Assets/Scripts/PlayerScripts/EnemyBehaviour.cs
@@ -1,12 +1,45 @@
+using System;
 using UnityEngine;
 
 public abstract class EnemyBehaviour : MonoBehaviour
 {
 
     public float EnemyHealth = 30;
-    public float Enemydamage = 20;
+    public float EnemyDamage = 20;
     public float EnemySpeed = 10;
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+    public virtual void Attack()
+    {
+        if (Vector3.Distance(gameObject.transform.position, player.transform.position) < 10)
+        {
+            player.GetComponent<PlayerBehaviour>().Health -= EnemyDamage;
+        }
+
+    }
+    private void Update()
+    {
+        Console.WriteLine(Vector3.Distance(gameObject.transform.position, player.transform.position));
+        Dead();
+    }
+
+    public virtual void Dead()
+    {
+        if (EnemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "")
+    //}
 
 
-    
+
 }
